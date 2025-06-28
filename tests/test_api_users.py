0922499381248699631d8700c2b11704abaae2c8
Vasiliy Shalaev <110035787+Younezzzz@users.noreply.github.com>
2025-06-29 00:28:48 +0300

    Krutie testy

--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -20,3 +20,9 @@ def test_create_user_status_code(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.new_user({"job":"Chlenosos","name":"Ayan"})
     assert response.status_code == 201
+
+@pytest.mark.usefixtures("headers")
+def test_create_user(api_url, headers):
+    api = UserAPi(api_url, headers)
+    response = api.new_user({"job":"krutoi_qa_prosto_legenda","name":"Vasya"}).json()
+    assert (response['job'] == "krutoi_qa_prosto_legenda" and response['name']=="Vasya")
