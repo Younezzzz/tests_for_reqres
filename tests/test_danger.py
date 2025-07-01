69949317a16d6593e226e87d91e7056b1c1b398f
Vasiliy Shalaev <110035787+Younezzzz@users.noreply.github.com>
2025-07-02 02:18:27 +0300

    dispatch

--- tests/test_danger.py
+++ tests/test_danger.py
@@ -2,17 +2,19 @@ import pytest
 from api.user_api import UserAPi
 from api.schema import *
 
-
+@pytest.mark.danger
 def test_create_user_status_code(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.new_user({"job":"Chlenosos","name":"Ayan"})
     assert response.status_code == 201
 
+@pytest.mark.danger
 def test_create_user(api_url, headers):
     api = UserAPi(api_url, headers)
     response = api.new_user({"job":"krutoi_qa_prosto_legenda","name":"Vasya"}).json()
     assert UserSchema(**response)
 
+@pytest.mark.danger
 def test_update_job_user(api_url, headers):
     api = UserAPi(api_url, headers)
     response = api.update_user(2,'Ayan','bedolaga')
--- tests/test_safe.py
+++ tests/test_safe.py
@@ -3,48 +3,56 @@ from api.user_api import UserAPi
 from api.schema import *
 
 
-@pytest.mark.api
+@pytest.mark.safe
 def test_get_users_status_code(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_all_users(2)
     assert response.status_code == 200
 
+@pytest.mark.safe
 def test_get_users(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_all_users(2).json()
     assert UsersListSchema(**response)
 
+@pytest.mark.safe
 def test_get_users_by_id_status_code(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_user_by_id(1)
     assert response.status_code == 200
 
+@pytest.mark.safe
 def test_get_users_by_id(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_user_by_id(1).json()
     assert SingleUser(**response)
 
+@pytest.mark.safe
 def test_user_not_foud(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_user_by_id(23)
     assert response.status_code == 404
 
+@pytest.mark.safe
 def test_resources_status_code(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_all_resources()
     assert response.status_code == 200
 
+@pytest.mark.safe
 def test_resources(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_all_resources()
     assert ResourceListSchema(**response.json())
 
+@pytest.mark.safe
 def test_one_resource(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_resource_by_id(1)
     print(response.json())
     assert SingleResource(**response.json())
 
+@pytest.mark.safe
 def test_unknown_id(api_url, headers):
     api = UserAPi(api_url, header=headers)
     response = api.get_resource_by_id(23)
